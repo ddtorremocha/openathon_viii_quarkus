fd12913daa22aa78a92c497ae1d86a4a9c3d96e2
josdev27 <47904226+josdev27@users.noreply.github.com>
2021-03-13 19:50:40 +0100

    Create init-pwd.sh

---
+++ resources/init-pwd.sh
@@ -0,0 +1,18 @@
+apk add openjdk11 maven
+export JAVA_HOME=/usr/lib/jvm/default-jvm
+
+java --version
+mvn --version
+
+# Descargar el paquete
+wget https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.4.3/spring-boot-cli-2.4.3-bin.tar.gz
+
+# Descomprimimos el tar. Crea la carpeta spring-2.4.3
+tar -zxf spring-boot-cli-2.4.3-bin.tar.gz
+
+# Ejemplo de uso:
+# Creamos una aplicación Spring Boot con la dependencia web
+./spring-2.4.3/bin/spring init --dependencies=web my-project
+
+kill -9 $(pidof sshd)
+/usr/sbin/sshd -o AllowTcpForwarding=yes -o PermitRootLogin=yes

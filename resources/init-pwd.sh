3f8f289108514295a9b4fbf8f88927f4f3474933
josdev27 <47904226+josdev27@users.noreply.github.com>
2021-03-13 20:56:35 +0100

    Update init-pwd.sh

--- resources/init-pwd.sh
+++ resources/init-pwd.sh
@@ -11,10 +11,13 @@ wget https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.4
 
 # Descomprimimos el tar. Crea la carpeta spring-2.4.3
 tar -zxf spring-boot-cli-2.4.3-bin.tar.gz
+rm -rf spring-boot-cli-2.4.3-bin.tar.gz
+
+export PATH=$HOME/spring-2.4.3/bin/spring:$PATH
 
 # Ejemplo de uso:
 # Creamos una aplicación Spring Boot con la dependencia web
-./spring-2.4.3/bin/spring init --dependencies=web my-project
+spring init --dependencies=web my-project
 
 kill -9 $(pidof sshd)
 /usr/sbin/sshd -o AllowTcpForwarding=yes -o PermitRootLogin=yes
